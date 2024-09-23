694a5ce3fb739b873667460f4d29dcf2b4bb6034
madhumita1301 <162785036+madhumita1301@users.noreply.github.com>
2024-09-23 19:56:21 +0530

    Create ToDoList.java

---
+++ ToDoList.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoList {
+    private List<Task> tasks;
+
+    public ToDoList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(String description) {
+        tasks.add(new Task(description));
+    }
+
+    public void editTask(int index, String newDescription) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).setDescription(newDescription);
+        } else {
+            System.out.println("Task index out of range.");
+        }
+    }
+
+    public void removeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+        } else {
+            System.out.println("Task index out of range.");
+        }
+    }
+
+    public void displayTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks to display.");
+            return;
+        }
+
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+    }
+}
+
+// compile using 
+//          javac Task.java ToDoList.java ToDoApp.java
+// run the main class
+//          java ToDoApp
